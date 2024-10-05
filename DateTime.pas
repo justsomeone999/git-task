d26682872de62e32f2eb36f0542739079fc0b650
Maria Ligorova <masaligorova@gmail.com>
2024-10-05 14:33:53 +0300

    Add count days in Year

--- DateTime.pas
+++ DateTime.pas
@@ -1,22 +1,26 @@
 ﻿begin
-  var flag:=True;
-  var N:=ReadInteger('Введите год:');
-  if (N mod 4=0) and (N mod 100<>0) then
-    flag:=True
-  else if N mod 400=0 then flag:=True
+  var flag := True;
+  var N := ReadInteger('Введите год:');
+  if (N mod 4 = 0) and (N mod 100 <> 0) then
+    flag := True
+  else if N mod 400 = 0 then flag := True
   else
-    flag:=False;
+    flag := False;
   Print($'Год является високосным? {flag}');
-  var (Day1, Day2):=ReadInteger2('Введите дату двух ней:');
-  var (Month1, Month2):=ReadInteger2('Введите дату двух месяцев:');
-  if Month1>Month2 then
+  if flag then 
+    Print('В году 366 дней')
+  else 
+    Print('В году 365 дней');
+  var (Day1, Day2) := ReadInteger2('Введите дату двух ней:');
+  var (Month1, Month2) := ReadInteger2('Введите дату двух месяцев:');
+  if Month1 > Month2 then
     Print(1)
-  else if Month1<Month2 then
+  else if Month1 < Month2 then
     Print(2)
-  else if Day1<Day2 then
+  else if Day1 < Day2 then
     Print(2)
-  else if Day1>Day2 then
+  else if Day1 > Day2 then
     Print(1)
   else 
-end.
     Print('Даты одинаковы');
+end.
