50e30934cad5821f321ffa47ccdc68fe5e027e1a
Maria Ligorova <masaligorova@gmail.com>
2024-10-05 14:14:35 +0300

    Add what day is closer to New Year

--- DateTime.pas
+++ DateTime.pas
@@ -6,5 +6,17 @@
   else if N mod 400=0 then flag:=True
   else
     flag:=False;
-  Print($'Год является високосным? {flag}')
+  Print($'Год является високосным? {flag}');
+  var (Day1, Day2):=ReadInteger2('Введите дату двух ней:');
+  var (Month1, Month2):=ReadInteger2('Введите дату двух месяцев:');
+  if Month1>Month2 then
+    Print(1)
+  else if Month1<Month2 then
+    Print(2)
+  else if Day1<Day2 then
+    Print(2)
+  else if Day1>Day2 then
+    Print(1)
+  else 
 end.
+    Print('Даты одинаковы');
